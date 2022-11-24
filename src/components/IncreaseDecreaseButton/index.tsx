2710cc457030d2b4a164b45ea492cf7ef2110bae
gevored <gersonrevoredo@hotmail.com>
2022-11-23 23:55:24 -0300

    button state increase and descrease item done

--- src/components/IncreaseDecreaseButton/index.tsx
+++ src/components/IncreaseDecreaseButton/index.tsx
@@ -1,10 +1,20 @@
 import { ButtonStyles, ButtonStylesContainer } from './stylex'
+import { useState } from 'react'
+
 export function IncreaseDecreaseButton() {
+  const [numberOfCafes, setNumberOfCafes] = useState(0)
+  function handleChangeQuantity(qtd: number) {
+    const auxQtd = numberOfCafes + qtd
+    if (auxQtd >= 0) {
+      console.log('epa')
+      setNumberOfCafes(numberOfCafes + qtd)
+    }
+  }
   return (
     <ButtonStylesContainer>
-      <ButtonStyles>-</ButtonStyles>
-      <span>1</span>
-      <ButtonStyles>+</ButtonStyles>
+      <ButtonStyles onClick={() => handleChangeQuantity(-1)}>-</ButtonStyles>
+      <span>{numberOfCafes}</span>
+      <ButtonStyles onClick={() => handleChangeQuantity(1)}>+</ButtonStyles>
     </ButtonStylesContainer>
   )
 }
